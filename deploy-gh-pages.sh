5c712bec2914ade3803755184bf6b82d43ed79d3
SebastianJay <jgs3cd@virginia.edu>
2017-03-14 15:54:20 -0400

    update deploy script

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# make new directory with built files
+cd ..
+cp -a sgd-docs built
+cd built
+git init
+
 # make a commit and deploy to github pages
 git config user.name "Travis"
 git config user.email "sgdatuva@gmail.com"
