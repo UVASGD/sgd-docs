481be40645aee7b5870559564ffa72cbbb624b89
SebastianJay <jgs3cd@virginia.edu>
2017-03-14 23:37:23 -0400

    remove generated files on master

--- Scripts/deploy-gh-pages.sh
+++ Scripts/deploy-gh-pages.sh
@@ -3,7 +3,12 @@
 # make a commit on gh-pages branch and push to deploy
 cd ..
 git clone --depth=50 --branch=gh-pages https://github.com/UVASGD/sgd-docs.git sgd-docs-ghp
-rsync -avm --include='*.html' --filter='hide,! */' sgd-docs/ sgd-docs-ghp
+rsync -avm --include='*.html' \
+	--include '*.png' \
+	--include '*.jpg' \
+	--include '*.css' \
+	--include '*.ico' \
+	--filter='hide,! */' sgd-docs/ sgd-docs-ghp
 cd sgd-docs-ghp
 git config user.name "Travis"
 git config user.email "sgdatuva@gmail.com"
