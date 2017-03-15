a507aaa7fc0b4c931a905fda74fe9e562d600b88
SebastianJay <jgs3cd@virginia.edu>
2017-03-14 23:42:22 -0400

    remove scripts on gh-pages

--- Scripts/deploy-gh-pages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# make a commit and deploy to github pages
-git checkout --track origin/gh-pages
-git config user.name "Travis"
-git config user.email "sgdatuva@gmail.com"
-git add .
-git commit -m "Travis: generate HTML pages [ci skip]"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" > /dev/null 2>&1
