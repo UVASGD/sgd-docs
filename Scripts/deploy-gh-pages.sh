889dd9581bcf8426079acf21afaf9cd79604b3c1
SebastianJay <jgs3cd@virginia.edu>
2017-03-14 22:25:51 -0400

    use new branch, require sudo

--- Scripts/deploy-gh-pages.sh
+++ Scripts/deploy-gh-pages.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# make new directory with built files
-#cd ..
-#cp -a sgd-docs built
-#cd built
-#git init
-
 # make a commit and deploy to github pages
-git checkout master
+git checkout --track origin/gh-pages
 git config user.name "Travis"
 git config user.email "sgdatuva@gmail.com"
 git add .
